da526e2ac27813e42416227e7657abc7105d6d3a
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-11 03:30:12 -0300

    Ejercicios de la facu

--- 2-Estructura alternativa/Alumno.pas
+++ 2-Estructura alternativa/Alumno.pas
@@ -3,12 +3,12 @@
 // Aprueba con 6
 
 
-Program courseUdemy;
+Program Alumno;
 Var
 	nota:byte;
 	
 Begin
-	writeln('ingrese la nota'); readln(nota);
+	writeln('ingrese la nota del final'); readln(nota);
 	begin
 		if ( nota >= 4) then
 		write('el alumno esta aprobado')
@@ -17,11 +17,11 @@ Begin
 		end;
 	Begin 
 		If (nota >= 9) then
-		write(',es un prodigio')
+		write(',es un alumno sobresaliente')
 	end;
 	Begin
 		If ( nota <= 3 ) then 
-		writeln(', recursa');
+		writeln(', el alumno debe recursar');
 	end;
 
 end.
