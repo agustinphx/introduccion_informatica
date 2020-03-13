f187104e56392a2041d46e60be5fea24b146f5f8
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-12 23:30:53 -0300

    Ejercicios de la facu

--- 1-Intro estructura secuencial/tiempo.pas
+++ 1-Intro estructura secuencial/tiempo.pas
@@ -1,6 +1,6 @@
 //4-Escriba un programa donde se ingrese el tiempo necesario para un cierto proceso en horas, minutos y segundos. 
 //Calcule el costo total del proceso sabiendo que el costo por segundo es 0,25$. (Debe salir por pantalla el
-//tiempo expresado en horas, minutos y segundos, el costo por segundo y el costo total)
+//tiempo expresado en horas, minutos ,segundos y el costo total)
 Program tiempo4;
 Var
 	horas,minutos,segundos:byte;
