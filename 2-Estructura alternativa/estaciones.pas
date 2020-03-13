d6795cf01fea1f29a84ba78e86e6dedade1f3d91
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-13 15:14:18 -0300

    Ejercicios de la facu

---
+++ 2-Estructura alternativa/estaciones.pas
@@ -0,0 +1,21 @@
+//Ingresar una fecha ( dia y mes) informar a qué estación pertenece.
+Program estaciones;
+Var
+	mes,dia:byte;
+Begin
+write('Ingrese el dia : ');readln(dia);
+write('Ingrese el mes : ');readln(mes);
+
+If(mes = 12) and (dia >= 21) or (mes <= 3) and (dia < 21) then
+	writeln('Es verano');
+
+If(mes = 3) and (mes < 6) and (dia >= 21) or (mes > 3) and (mes <= 6) and (dia < 21) then
+	writeln('Es otonio');
+
+If(mes = 6) and (dia >= 20) or (mes > 6) and (mes <= 9) and (dia < 21) then
+	writeln('Es invierno');
+
+If(mes = 9) and (dia >= 21) or (mes > 9) and (mes <= 12) and (dia < 21) then
+	writeln('Es primavera');
+	
+end.
