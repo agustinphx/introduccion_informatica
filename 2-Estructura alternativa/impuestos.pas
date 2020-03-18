ea1f0811b500778458ca77842fe4a607d45a5776
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-18 00:38:46 -0300

    Ejercicios de la facu

---
+++ 2-Estructura alternativa/impuestos.pas
@@ -0,0 +1,31 @@
+//Desarrollar un programa para aplicar un ajuste anual por pago atrasado de impuestos inmobiliarios a un contribuyente.
+//El incremento depende del año que paga. Si el año es anterior a 1980, se venció la
+//obligación y no adeuda importe alguno, en otros casos:
+//- 1980 a 1990 Se aplica un recargo del 200% .
+//- 1991 a 1999 Se aplica un recargo del 100%.
+//- 2000 a 2009 Se aplica un recargo del 70%.
+//- 2010 a 2015 Se aplica un recargo del 30 %.
+Program impuestos;
+Var
+	anio:longint;
+	deuda:real;
+Begin
+write('Ingrese el anio de su deuda : ');readln(anio);
+write('Ingrese el monto de su deuda : ');readln(deuda);
+
+If (anio < 1980) then
+	write('Su deuda prescribio')
+Else if (anio >= 1980) and (anio <= 1990) then
+	deuda:= deuda * 2
+	
+Else if (anio > 1990) and (anio <= 1999) then
+	deuda:= deuda + deuda
+	
+Else if (anio > 1999) and (anio <= 2009) then
+	deuda:= deuda * 1.7
+
+Else if (anio > 2009) and (anio <= 2015) then
+	deuda:= deuda * 1.3;
+	
+writeln('Su deuda es : ',deuda:2:0);
+end.
