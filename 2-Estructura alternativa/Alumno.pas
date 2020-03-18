b252e756d7f2e5769b81db21a9ebd92fcff0d25e
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-18 00:54:56 -0300

    Ejercicios de la facu

--- 2-Estructura alternativa/Alumno.pas
+++ 2-Estructura alternativa/Alumno.pas
@@ -5,21 +5,21 @@ Program Alumno;
 Var
 	nota:byte;
 Begin
-	writeln('ingrese la nota del final'); readln(nota);
-	begin
-		if ( nota >= 4) then
-		write('el alumno esta aprobado')
-	Else
+writeln('ingrese la nota del final'); readln(nota);
+begin
+if ( nota >= 4) then
+	write('el alumno esta aprobado')
+Else
 		write('el alumno esta desaprobado');
-		end;
-	Begin 
-		If (nota >= 9) then
-		write(',es un alumno sobresaliente')
-	end;
-	Begin
-		If ( nota <= 3 ) then 
-		writeln(', el alumno debe recursar');
-	end;
+end;
+Begin 
+If (nota >= 9) then
+	write(',es un alumno sobresaliente')
+end;
+Begin
+	If ( nota <= 3 ) then 
+	writeln(', el alumno debe recursar');
+end;
 
 end.
 	
