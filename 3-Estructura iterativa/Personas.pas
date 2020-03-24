e4847f281809d86360a8b85a0fdaaa618743f32c
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-23 23:37:00 -0300

    Ejercicios de la facu

--- 3-Estructura iterativa/Personas.pas
+++ 3-Estructura iterativa/Personas.pas
@@ -36,11 +36,9 @@ Begin
 	promM:= EdadM / ContM; //Dividimos todas las edades ingresadas en masculino por la cantidad de estas.
 	EdadTotal:= EdadF + EdadM; // Sumamos todas las edades.
 	PromTotal:= EdadTotal / N; //Dividimos el total de las edades con el total de personas para obtener la edad promedio.
-	writeln('El porcentaje de mujeres es del: ',ContF / N *100:2:0,' %');
-	writeln('El promedio de la edad de las mujeres es: ',promF:2:0);  
+	writeln('El porcentaje de mujeres es del: ',ContF / N *100:2:0,' %  y su  promedio de edad es: ',promF:2:0);
 	writeln(' '); 
-	writeln('El porcentaje de hombres es del: ',ContM / N *100:2:0,' %');
-	writeln('El promedio de la edad de los hombres es: ',promM:2:0); 
+	writeln('El porcentaje de hombres es del: ',ContM / N *100:2:0,' % y su promedio de edad es: ',promM:2:0);
 	writeln(' ');
 	writeln('El promedio general de  edad es: ',promTotal:2:0); 
 end.
