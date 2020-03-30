e564e1cdbc64d51823d2f04537b7345527c57b24
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-03-30 02:57:41 -0300

    Ejercicios de la facu

--- 2-Estructura alternativa/mayorista.pas
+++ 2-Estructura alternativa/mayorista.pas
@@ -1,88 +1,31 @@
 {Un mayorista vende huevos y los cobra de la siguiente forma:
 $5 la primera docena,
 del sobrante $3 cada 10 unidades,
-de lo que sobra $2 cada 5 unidades
-y luego $1 por unidad}
+de lo que sobra $2 cada 5 unidades y luego $1 por unidad}
 Program mayorista;
 Var
-	cantidad:byte;
-	precio,sobrante:real;
+	cantidad,sobrante:byte;
+	precio:real;
 Begin	
-	sobrante:= 0;
+
 	write('Ingrese la cantidad de huevos: ');readln(cantidad);
-	
-	Case cantidad of
-		1..12: precio:= cantidad * 5;
-		13..22: precio:= cantidad * 3;
-		23..28: precio:= cantidad * 2;
-	end;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	{If(cantidad <= 12) then
+	If(cantidad >= 12 ) then
 	begin
-		precio:= cantidad * 5;
-		
+		precio:= 5;
+		sobrante:= cantidad - 12;
 	end
-	Else 
-		if (cantidad <= 22) then
-		begin
-			sobrante:= (cantidad - 12) * 3;
-			precio:= (cantidad * 5)  + sobrante; 
-		end
-		Else
-			If (cantidad <= 27) then 
-			begin
-			sobrante:= (cantidad - 22) * 2;
-			precio:= (cantidad * 4)  + sobrante;
-				
-			end
-			Else
-				precio:= cantidad;	}
-				
-				
-	
-			
-				
-	{If (sobrante <= 10) then
-	begin	
-		sobrante:= sobrante * 3;
-		precio:= precio + sobrante;
-	end	
 	Else
-		if (sobrante <= 5) then
-		begin
-			sobrante:= sobrante * 2;
-			precio:= precio + sobrante;
-		end
+		sobrante:= 12 - cantidad;
+		
+	If (sobrante MOD 10 >= 0) then
+		precio:= precio + (cantidad DIV 10)*3
+	Else
+		if (sobrante MOD 5 >= 0) then
+			precio:= precio + (cantidad DIV 5)*2
 		Else
-			sobrante:= sobrante;
-			precio:= precio + sobrante;}
-			
-			
-			
-			
-			
-	//writeln('El sobrante es: ',sobrante:2:0);
-	writeln('El precio a pagar es: $',precio:2:0);
+			precio:= precio;
+
+	writeln('El sobrante es: ',sobrante); //Añadi esta linea para testear si contaba bien la cantidad sobrante.
+	writeln('El precio es: $',precio:2:0);
+	
 end.
