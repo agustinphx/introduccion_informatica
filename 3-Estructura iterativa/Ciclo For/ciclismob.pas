d21d80ac8596b175235b2cea91c99ba03483c206
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-04-01 16:50:43 -0300

    Ejercicios de la facu

--- 3-Estructura iterativa/Ciclo For/ciclismob.pas
+++ 3-Estructura iterativa/Ciclo For/ciclismob.pas
@@ -1,82 +1,80 @@
 {15-En un entrenamiento de ciclismo cada participante, efectúa una vuelta a la pista y al
 terminar la misma se registra el nombre (*** fin de datos), la velocidad máxima alcanzada y
 sus pulsaciones.
-Se pide desarrollar un programa Pascal que lea de teclado los datos mencionados, obtenga e
-informe:
- La categoría de cada ciclista y su nivel de riesgo
- La cantidad de participantes en cada categoría
- El porcentaje de participantes cuyo de nivel de riesgo es 3
 Hasta 120 pulsaciones  nivel riesgo 1
 Entre 121 y 160 nivel 2
 Mas de 160  nivel 3
 
 Velocidad: mas de 50kmh categoria A
-* 		   entre 40 y 45km/h categoria B
-* 			menos de 40km/h categoria C
+ 		   entre 40 y 45km/h categoria B
+ 			menos de 40km/h categoria C
+Se pide desarrollar un programa Pascal que lea de teclado los datos mencionados, obtenga e
+informe:
 a. Promedio de velocidades máximas
 b. Cantidad de corredores de categoría A con nivel de riesgo 1
 c. Informar categorías sin participantes}
 Program eje15b;
 Var
-	Cont,ContA,ContB,ContC,N,i,SumA:word;
-	vel,pul:SmallInt;
-	PromA:real;
+	Cont,ContV,ContA,ContB,ContC,N,i,SumA,SumV:word;
+	vel,pul,velMax:SmallInt;
+	Prom:real;
 Begin
 	Cont:= 0;
+	ContV:= 0;
 	ContA:= 0;
 	ContB:= 0;
 	ContC:= 0;
 	SumA:= 0;
+	velMax:= -9999;
+	SumV:= 0;
 	write('Ingrese la cantidad de participantes: ');readln(N);
 	For i:= 1 to N do
 	begin
 		writeln(' ');
 		write('Cuantas pulsaciones tuvo? ');readln(pul);
-		write('Cual fue su velocidad maxima en km/h? ');readln(vel);	
-		If (pul <= 120) then
-		begin
-			writeln('Este ciclista pertenece al nivel de riesgo 1');
-		end
-		Else
-			if (pul <= 160) then
-				writeln('Este ciclista pertenece al nivel de riesgo 2')
-			Else
-				writeln('Este ciclista pertenece al nivel de riesgo 3');
+		write('Cual fue su velocidad maxima en km/h? ');readln(vel);
 				
-		If (pul <= 120) and (vel > 50) then //Cuenta la cantidad de corredores de categoria A con nivel de riesgo 1.
+		If (pul <= 120) and (vel > 50) then //Cuenta exclusivamente la cantidad de ciclistas de categoria A con nivel de riesgo 1.
 			Cont:= Cont + 1;
-			
-		If (vel > 50) then //Cuenta solamente la categoria A independiente del nivel de riesgo.
+	
+		If (vel > 50) then //Empezamos a contar,si cumple la condición, los ciclistas de cada categoria independiente del nivel de riesgo.
 		begin
 			ContA:= ContA + 1;
 			SumA:= SumA + vel;
 		end
 		Else 
-			if (vel > 40) and (vel <= 49) then //Cuenta solamente la categoria B independiente del nivel de riesgo.
+			if (vel > 40) and (vel <= 49) then  //Si no ponia el > 40 nunca entraba a sumar en el ContC.
 				ContB:= ContB + 1
 			Else
-				ContC:= ContC + 1; //Cuenta solamente la categoria C independiente del nivel de riesgo.
+				ContC:= ContC + 1; 
+				
+		If (velMax < vel) then //Calcula las velocidades maximas
+		 begin	
+		 	velMax:= vel;
+		 	SumV:= SumV + vel;
+		 	ContV:= ContV + 1;
+		 end;		 
 	end;
 	writeln(' ');
-	If (ContA = 0) then //Si al finalizar el ciclo,una vez que ya se ingresaron los datos, no se contaron ciclistas de la categoria A entonces imprimimos que no hay participantes en dicha categoria.
+	If (ContA = 0) then //Si al finalizar el ciclo,una vez que ya se ingresaron los datos, no se contaron ciclistas de alguna categoria entonces imprimimos que no hay participantes en dicha categoria.
 		begin
 			ContA:= ContA;
 			writeln('La categoria A no tiene participantes');
 		end
 		Else
-			if (ContB = 0) then //Si al finalizar el ciclo,una vez que ya se ingresaron los datos, no se contaron ciclistas de la categoria B entonces imprimimos que no hay participantes en dicha categoria.
+			if (ContB = 0) then 
 			begin
 				ContB:= ContB;
 				writeln('La categoria B no tiene participantes');
 			end
 			Else
 			begin
-				ContC:= ContC; //Igual que con las dos categorias anteriores.
+				ContC:= ContC; 
 				writeln('La categoria C no tiene participantes');
 			end;
 
-	PromA:= SumA / ContA; // Promedio de las velocidades maximas.
-	writeln('El promedio de velocidades maximas es: ',PromA:2:0);
+	Prom:= SumV/ ContV; // Promedio de las velocidades maximas.
+	writeln('El promedio de velocidades maximas es: ',Prom:2:0);
 	writeln('La cantidad de corredores de categoria A con nivel de riesgo 1 son: ',Cont);
 		
 end.
