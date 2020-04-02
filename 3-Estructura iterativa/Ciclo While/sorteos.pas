6d3ae568afbee4497e6ea10203fb1025223aff22
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-04-02 02:44:16 -0300

    Ejercicios de la facu

--- 3-Estructura iterativa/Ciclo While/sorteos.pas
+++ 3-Estructura iterativa/Ciclo While/sorteos.pas
@@ -1,4 +1,4 @@
-{-En un sorteo de lotería se extraen 6 números enteros entre 1 a 40.
+{-En un sorteo de lotería se extraen los numeros 2,5,1,19,3,12,3,13,9,1,7,29,2,8,1,14,37,16. 
 Se pide ingresar los datos de cada uno de los últimos N sorteos e informar:
 a) La cantidad de sorteos que han tenido al menos un número impar.
 b) El número par más alto de cada sorteo, indicar cuándo no haya habido }
