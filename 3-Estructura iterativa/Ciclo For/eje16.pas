2536c8918718ff0a800ea77dd2be3fc03e9f4cc2
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-04-01 21:04:13 -0300

    Ejercicios de la facu

--- 3-Estructura iterativa/Ciclo For/eje16.pas
+++ 3-Estructura iterativa/Ciclo For/eje16.pas
@@ -7,22 +7,23 @@ b) Conocer la Distancia Media de la prueba.}
 Program eje16;
 Var
 	i,N,Cont:word;
-	Dni:longint;
+	Dni,DniM:longint;
 	Media,Dis,DisMax,Acum:real;
 Begin
 	Cont:= 0;
 	Acum:= 0;
 	DisMax:= -9999;
+	DniM:= -9999;
 	write('Ingrese la cantidad de deportistas: ');readln(N);
 	For i:= 1 to N do
 	begin
 		write('Ingrese el numero de DNI: ');readln(Dni);
 		write('Ingrese la distancia del lanzamiento: ');readln(Dis);
 		
-		If (DisMax > Dis) then //Calcula la distancia maxima.
+		If (DisMax < Dis) then //Calcula la distancia maxima.
 		begin	
 			DisMax:= Dis;
-			Dni:= Dni; // Ganador igual a ese dia que obtuvo la maxima distancia.
+			DniM:= Dni; // Ganador igual a ese dia que obtuvo la maxima distancia.
 		end;
 		
 		If (Dis > 0) then //Acumula y cuenta todas las distancias.
@@ -33,6 +34,6 @@ Begin
 		writeln(' ');
 	end;
 	Media:= Acum / Cont; //Calcula la media entre todas las distancias.
-	writeln('El dni ganador es: ',Dni);
+	writeln('El dni ganador es: ',DniM,' y su distancia fue: ',DisMax:4:2);
 	writeln('La distancia media es: ',media:2:2);
 end.
