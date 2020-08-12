29c6219f7b7b916a372d7349873bcbc4e112bad1
Arp-3 <58674979+Arp-3@users.noreply.github.com>
2020-08-11 23:32:44 -0300

    Procedures - Functions - Some Arrays

---
+++ 4- Procedimientos y funciones/fracciones.pas
@@ -0,0 +1,24 @@
+{A partir de dos fracciones devolver la fracción suma. ¿Podría utilizarse una función que
+realice la misma operación? }
+// No se puede utilizar una funcion porque devuelve más de un resultado.
+Program fracciones;
+Var
+	X,Y,W,Z,Frac1,Frac2:real;
+	
+Procedure DevolverSuma(A,B,C,D:real);	
+Begin	
+	Frac1:= D*A + B*C;
+	Frac2:= B*D;
+	writeln('El numerador es: ' ,Frac1:2:0);
+	writeln('El denominador es: ' ,Frac2:2:0);
+end;
+
+Begin
+	write('Ingrese el primer numerador: ');readln(X);
+	write('Ingrese el primer denominador: ');readln(Y);
+	writeln;
+	write('Ingrese el segundo numerador: ');readln(W);
+	write('Ingrese el segundo denominador: ');readln(Z);
+	writeln;
+	DevolverSuma(X,Y,W,Z);
+end.
